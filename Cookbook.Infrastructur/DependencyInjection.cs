ffb0a463a1faf003e4764a99b84080fd41fe9625
aft <aft@captureone.com>
2024-01-13 15:17:59 +0100

    DI for reposittory

---
+++ Cookbook.Infrastructur/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Cookbook.Application;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cookbook.Infrastructur
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services)
+        {
+            services.AddSingleton<ICookbookRepository, CookbookRepository>();
+
+            return services;
+        }
+
+    }
+}

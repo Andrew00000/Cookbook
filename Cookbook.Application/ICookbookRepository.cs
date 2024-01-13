a8a6f21834412c24e7aaf66dda0ef1b11ac3640b
aft <aft@captureone.com>
2024-01-13 15:17:30 +0100

    Added in memory repository

---
+++ Cookbook.Application/ICookbookRepository.cs
@@ -0,0 +1,13 @@
+﻿using Cookbook.Domain;
+
+namespace Cookbook.Application
+{
+    public interface ICookbookRepository
+    {
+        Task<bool> CreateAsync(Recipe recipe);
+        Task<Recipe?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Recipe>> GetAllAsync();
+        Task<bool> UpdateByIdAsync(Recipe recipe);
+        Task<bool> DeleteByIdAsync(Guid id);
+    }
+}
---
+++ Cookbook.Infrastructur/CookbookRepository.cs
@@ -0,0 +1,49 @@
+﻿using Cookbook.Application;
+using Cookbook.Domain;
+
+namespace Cookbook.Infrastructur
+{
+    public class CookbookRepository : ICookbookRepository
+    {
+        private readonly List<Recipe> recipes = new();
+        public Task<bool> CreateAsync(Recipe recipe)
+        {
+            recipes.Add(recipe);
+            return Task.FromResult(true);
+        }
+
+        public Task<Recipe?> GetByIdAsync(Guid id)
+        {
+            var recipe = recipes.SingleOrDefault(x => x.Id == id);
+            return Task.FromResult(recipe);
+        }
+        public Task<IEnumerable<Recipe>> GetAllAsync()
+        {
+            return Task.FromResult(recipes.AsEnumerable());
+        }
+
+
+        public Task<bool> UpdateByIdAsync(Recipe recipe)
+        {
+            var recipeIndex = recipes.FindIndex(x => x.Id == recipe.Id);
+
+            if (recipeIndex == -1)
+            {
+                return Task.FromResult(false);
+            }
+
+            recipes[recipeIndex] = recipe;
+
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> DeleteByIdAsync(Guid id)
+        {
+            var removedCount = recipes.RemoveAll(x => x.Id == id);
+
+            var recipeRemoved = removedCount > 0;
+
+            return Task.FromResult(recipeRemoved);
+        }
+    }
+}

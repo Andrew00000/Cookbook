045fe71cb2a976882388d773b3cb7ba8dafdb8f1
aft <aft@captureone.com>
2024-01-14 14:08:37 +0100

    Update Recipe.cs

--- Cookbook.Domain/Recipe.cs
+++ Cookbook.Domain/Recipe.cs
@@ -7,7 +7,7 @@
         public string Author { get; set; } = string.Empty;
         public required int Portions { get; set; }
         public int Calories { get; set; } = 0;
-        public required IEnumerable<string> Ingredients { get; init; }
+        public required IEnumerable<Ingredient> Ingredients { get; init; }
         public required IEnumerable<string> Steps { get; init; }
         public IEnumerable<string> Tags { get; init; } = Enumerable.Empty<string>();
     }

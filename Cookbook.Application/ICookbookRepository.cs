4f3e4a342717e87f47845bbb6b397a72d6446bc9
aft <aft@captureone.com>
2024-01-24 15:40:25 +0100

    Corrected incorrect parameters

--- Cookbook.Application/ICookbookRepository.cs
+++ Cookbook.Application/ICookbookRepository.cs
@@ -7,12 +7,12 @@ namespace Cookbook.Application
         Task<bool> CreateAsync(Recipe recipe);
         Task<IEnumerable<Recipe>> GetAllAsync();
         Task<bool> ExistsByIdAsync(Guid id);
-        Task<bool> ExistsBySlugAsync(Guid id);
+        Task<bool> ExistsBySlugAsync(string slug);
         Task<Recipe?> GetByIdAsync(Guid id);
         Task<Recipe?> GetBySlugAsync(string slug);
         Task<bool> UpdateByIdAsync(Recipe recipe);
         Task<bool> UpdateBySlugAsync(Recipe recipe);
         Task<bool> DeleteByIdAsync(Guid id);
-        Task<bool> DeleteBySlugAsync(Guid id);
+        Task<bool> DeleteBySlugAsync(string slug);
     }
 }

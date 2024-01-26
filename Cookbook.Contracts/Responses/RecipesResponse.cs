42f8bb234bbb937e5ca07bb45667177554d538e8
aft <aft@captureone.com>
2024-01-26 16:16:00 +0100

    Update RecipesResponse.cs

--- Cookbook.Contracts/Responses/RecipesResponse.cs
+++ Cookbook.Contracts/Responses/RecipesResponse.cs
@@ -2,6 +2,7 @@
 {
     public class RecipesResponse
     {
-        public required IEnumerable<RecipeResponse> Recipes { get; init; } = Enumerable.Empty<RecipeResponse>();
+        public required IEnumerable<RecipeResponse> Recipes { get; init; } = 
+                                            Enumerable.Empty<RecipeResponse>();
     }
 }

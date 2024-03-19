dbac8c9e7bf4c88682bf5aeccdd79556cbda4d33
Andrew <thefeka@gmail.com>
2024-03-19 15:20:37 +0100

    Update DbInitializer.cs

--- Cookbook.Repository/Database/Schema/DbInitializer.cs
+++ Cookbook.Repository/Database/Schema/DbInitializer.cs
@@ -23,15 +23,15 @@ namespace Cookbook.Repository.Database.Schema
                 {
                     await dbManipulator.RunQuery(async connection =>
                     {
-                        await connection.ExecuteAsync(new CommandDefinition(SqliteCommandTexts.CreateRecipesTable));
+                        await connection.ExecuteAsync(new CommandDefinition(SqliteCommandTexts.CreateRecipesTable, cts.Token));
 
-                        await connection.ExecuteAsync(new CommandDefinition(SqliteCommandTexts.CreateIngredientsTable));
+                        await connection.ExecuteAsync(new CommandDefinition(SqliteCommandTexts.CreateIngredientsTable, cts.Token));
 
-                        await connection.ExecuteAsync(new CommandDefinition(SqliteCommandTexts.CreateStepsTable));
+                        await connection.ExecuteAsync(new CommandDefinition(SqliteCommandTexts.CreateStepsTable, cts.Token));
 
-                        await connection.ExecuteAsync(new CommandDefinition(SqliteCommandTexts.CreateTagsTable));
+                        await connection.ExecuteAsync(new CommandDefinition(SqliteCommandTexts.CreateTagsTable, cts.Token));
 
-                        await connection.ExecuteAsync(new CommandDefinition(SqliteCommandTexts.CreateSlugIndex));
+                        await connection.ExecuteAsync(new CommandDefinition(SqliteCommandTexts.CreateSlugIndex, cts.Token));
                     }, cts.Token);
                 }
             }
